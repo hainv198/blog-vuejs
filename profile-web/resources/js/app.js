c0996e14da92976345bc6ca7b47d9cc8b62d1d94
MSI <hainvth161197@gmail.com>
2022-10-20 10:13:40 +0700

    create vue

--- profile-web/resources/js/app.js
+++ profile-web/resources/js/app.js
@@ -1,1 +1,7 @@
 import './bootstrap';
+// khai bao import model, khoi tao vue app va mount no vao mot element co id la app trong file blade in component vuejs
+import {createApp} from "vue";
+
+import app from './components/app'
+
+createApp(app).mount('#app')
---
+++ profile-web/resources/js/components/app.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Project profile vuejs</h1>
+</template>
